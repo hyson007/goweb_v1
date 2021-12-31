bc8f62d75f1363bb91e2c01c7b9110e88dea69e9
jack yao <hyson007@gmail.com>
2021-12-31 14:09:30 +0800

    using httprouter

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module goweb_v1
 
 go 1.17
+
+require github.com/julienschmidt/httprouter v1.3.0
--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func handleFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>welcome to my awesome site </h1>")
+}
+
+func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
+}
 
 func main() {
-	fmt.Println("hello World")
+	router := httprouter.New()
+	// router.GET("/", Index)
+	router.GET("/hello/:name", Hello)
+	// http.HandleFunc("/", handleFunc)
+	http.ListenAndServe(":3000", router)
+
 }
